d24884086fd193f0c50ee76f2005b98d84069c44
ljyGmail <liangjy881@gmail.com>
2024-12-27 22:16:01 +0900

    7.3 pointer operators

---
+++ 01_pointer_operators.c
@@ -0,0 +1,13 @@
+// Using the & and * pointer operators.
+#include <stdio.h>
+
+int main_701(void)
+{
+	int a = 7;
+	int* aPtr = &a; // set aPtr to the address of a
+
+	printf("Address of a is %p\nValue of aPtr is %p\n\n", &a, aPtr);
+	printf("Value of a is %d\nValue of *aPtr is %d\n\n", a, *aPtr);
+	printf("Showing that * and & are complements of each other\n");
+	printf("&*aPtr = %p\n*&aPtr = %p\n", &*aPtr, *&aPtr);
+}
