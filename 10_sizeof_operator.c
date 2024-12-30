8841d53f44c42105f0a3d40d11e4087c16ccf3a5
ljyGmail <liangjy881@gmail.com>
2024-12-31 01:27:07 +0900

    7.7 sizeof operator

---
+++ 10_sizeof_operator.c
@@ -0,0 +1,20 @@
+// Applying sizeof to an array name returns
+// the number of bytes in the array.
+#include <stdio.h>
+#define SIZE 20
+
+size_t getSize(const float* ptr);
+
+int main_710(void)
+{
+	float array[SIZE]; // create array
+
+	printf("Number of bytes in the array is %zu\n", sizeof(array));
+	printf("Number of bytes returned by getSize is %zu\n", getSize(array));
+}
+
+// return size of ptr
+size_t getSize(const float* ptr)
+{
+	return sizeof(ptr);
+}
